034a7ed1d81c76bf120654ea38a14bbe66d8d8eb
Sanjana Sangameshwar <sanjana.sangameshwar@gmail.com>
2019-06-13 11:19:26 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './header';
 import SideBar from './sideBar';
 
-function App() {
-  return (
-    <div className="app_container">
-      <Header/>
-      <div className="row" >
-        <SideBar className="col-sm-3"/>
-        <div className="app_content">
+// const base64 = 'ZWFjMmE5OGU0NjFjNGNjYmJmN2VmZTgzNDgwOTAxN2E6ZWVlYmM2YjJjYjg3NDEwYWE0NTdiZGUxYmE3NGI0MzM=';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app_container">
+        <Header />
+        <div className="row" >
+          <SideBar className="col-sm-3" />
+          <div className="app_content">
             dynamic content
             {/* {this.props.children} */}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
