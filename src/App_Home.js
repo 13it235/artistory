3d0c8799afae4b179dfbf2a90637fd56b29a64f6
Sanjana Sangameshwar <sanjana.sangameshwar@gmail.com>
2019-06-19 13:05:10 +0530

    chore(App_Home.js) : Removed hashHistory usage
    - using push property from react router redux for maintaning the history

--- src/App_Home.js
+++ src/App_Home.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { push } from 'react-router-redux';
 import './App.css';
-import { hashHistory } from 'react-router';
+
 import cookie from 'react-cookie';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 const authUrl = 'https://accounts.spotify.com/authorize?'
 const clientId = 'eac2a98e461c4ccbbf7efe834809017a';
@@ -17,33 +18,34 @@ class App_Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            login_url : ''
+            login_url: ''
         }
     }
 
     componentWillMount() {
+        var login_url = authUrl + 'client_id=' + clientId + '&response_type=token&redirect_uri='+ redirectURi + '&scope=' + scopes.join("%20") + '&state=34fFs29kd09'
         if (cookie.load('access_token')) {
-            hashHistory.push('/app');
+            push('/app');
         }
         else {
-            // window.location.assign(login_url)
-            // var urls = this.props.urls
+            window.location.assign(login_url)
+            // // var urls = this.props.urls
+            // // this.setState({
+            // //     login_url : urls.authUrl + 'client_id=' + urls.clientId + '&response_type=token&redirect_uri=' 
+            // //     + urls.redirectURi + '&scope=' + urls.scopes.join("%20") + '&state=34fFs29kd09'
+            // // })
             // this.setState({
-            //     login_url : urls.authUrl + 'client_id=' + urls.clientId + '&response_type=token&redirect_uri=' 
-            //     + urls.redirectURi + '&scope=' + urls.scopes.join("%20") + '&state=34fFs29kd09'
+            //     login_url: authUrl + 'client_id=' + clientId + '&response_type=token&redirect_uri='
+            //         + redirectURi + '&scope=' + scopes.join("%20") + '&state=34fFs29kd09'
             // })
-            this.setState({
-                login_url : authUrl + 'client_id=' + clientId + '&response_type=token&redirect_uri=' 
-                + redirectURi + '&scope=' + scopes.join("%20") + '&state=34fFs29kd09'
-            })
         }
     }
 
     render() {
         return (
             <div className='loginContainer'>
-                <div className='loginButton'
-                    onClick={() => { window.location.assign(this.state.login_url) }}>Login</div>
+                {/* <button className='loginButton'
+                    onClick={() => { window.location.assign(this.state.login_url) }}>Login</button> */}
             </div>
         )
     }
@@ -51,8 +53,8 @@ class App_Home extends Component {
 
 const mapStateToProps = (state) => {
     return {
-      urls: state.urls
+        urls: state.urls
     }
-  }
-  
-export default connect(mapStateToProps)(App_Home);
+}
+
+export default connect(mapStateToProps, null)(App_Home);
